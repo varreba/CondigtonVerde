c4813744074584c9e52a3b5fc2f2e6cdfb1b4b00
Varreba <varreab@gmail.com>
2015-02-16 10:55:22 +0100

    V 0.1 Prueba
    probando que funciona el repositorio

---
+++ src/es/ats/codington/festival/domain/prueba1.java
@@ -0,0 +1,13 @@
+package es.ats.codington.festival.domain;
+
+public class prueba1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
